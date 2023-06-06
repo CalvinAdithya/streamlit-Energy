e29c3d5ca33400059747ee66a10c02ca199f3f5a
CalvinAdithya <128107566+CalvinAdithya@users.noreply.github.com>
2023-06-06 22:49:40 +0700

    deploy1.py

--- deploy1.py
+++ deploy1.py
@@ -41,11 +41,9 @@ def autoplay_audio(file_path: str):
         html_code += f'<script>{js_code}</script>'
 
     html(html_code)
+    st.markdown("---")
     st.markdown(html_code, unsafe_allow_html=True)
-    
 autoplay_audio("sound.mp3")
-
-
 
 with st.sidebar :
     selected = option_menu('Big Project',
