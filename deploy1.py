8b0be6e33fcf855d38b4a8234edefb6152a40375
CalvinAdithya <128107566+CalvinAdithya@users.noreply.github.com>
2023-06-06 14:13:49 +0700

    deploy1.py

--- deploy1.py
+++ deploy1.py
@@ -205,3 +205,6 @@ if (selected =='Anggota') :
     st.write('Muhammad Calvin Adithya (2017031082)')
     st.write('Maya Puspitasari (2017031086)')
     st.write('Cindy Anisya (2017031074)')
+
+    image= Image.open('pp.jpg')
+    st.image(image)
