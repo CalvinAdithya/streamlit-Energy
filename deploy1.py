07e2528ebb16147112926c6ff533fc002a6b826b
CalvinAdithya <128107566+CalvinAdithya@users.noreply.github.com>
2023-06-12 07:53:32 +0700

    deploy1.py

--- deploy1.py
+++ deploy1.py
@@ -59,7 +59,7 @@ if (selected == 'ANN') :
     st.title("Prediksi Konsumsi Energi")
    
     country_input = st.selectbox("Pilih negara:", countries)
-    year_input = st.number_input("Year:", min_value=2022, step=1)
+    year_input = st.number_input("Year:", min_value=2021, step=1)
 
     # Mengeksekusi prediksi saat tombol dipencet
     if st.button("Prediksi"):
